5e7acb594a8aec8d595a9062ef6d3e0e30a6ce19
Joel Alexander <alexandj@etl.boi.rd.hpicorp.net>
2017-05-22 16:37:26 -0600

    first css redo commit

--- index.php
+++ index.php
@@ -6,25 +6,27 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
     <TITLE>Draft Helper</TITLE>
     <HEAD>
-        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu"/>
-        <link rel="stylesheet" href="/css/w3.css">
+        <!--<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu"/>
+        
+        <link rel="stylesheet" href="/css/w3.css">-->
+        <link rel="stylesheet" type="text/css" href="/css/reset.css">
         <link rel="stylesheet" type="text/css" href="/css/draft_helper.css">
         <script src='/js/jquery.js'></script>
         <script src='/js/draft.js'></script>
         <!--<script src='draft.js'></script>-->
     </HEAD>
     <body>
-            <div id='menu' class="w3-row">
-                <div class='menu_div w3-light-grey' id='draft_status'></div>
-                <div class='menu_div w3-light-grey' id='draft_controls'></div>
-                <div class='menu_div w3-light-grey' id='quick_draft_configuration'></div>
+            <div id='menu' >
+                <div id='draft_status'></div>
+                <div id='draft_controls'></div>
+                <div id='quick_draft_configuration'></div>
             </div>
             <div id="content">
-                <div id='players' class='w3-quarter'>
-                    <div id='player_list_filter' class='w3-panel'></div>
-                    <div id='player_list' class='w3-card-4'></div>
+                <div id='players'>
+                    <div id='player_list_filter' ></div>
+                    <div id='player_list'></div>
                 </div>
-                <div id='teams_display' class='w3-half w3-responsive'></div>
-                <div id='draft_record' class='w3-quarter w3-responsive'></div>
+                <div id='teams_display'></div>
+                <div id='draft_record'></div>
             </div>
         
